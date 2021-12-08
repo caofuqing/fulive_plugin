970e84c79556463706004f0622bb8b0c22a92f56
caofuqing <caofuqing@yxtreme.cn>
2021-12-08 11:25:56 +0800

    first commit

---
+++ ios/Classes/FulivePlugin.m
@@ -0,0 +1,20 @@
+#import "FulivePlugin.h"
+
+@implementation FulivePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fulive_plugin"
+            binaryMessenger:[registrar messenger]];
+  FulivePlugin* instance = [[FulivePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
