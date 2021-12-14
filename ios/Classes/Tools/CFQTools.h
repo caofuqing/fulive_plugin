57bfa472cd063d9b19c28b762e20b59017f4eae3
caofuqing <caofuqing@yxtreme.cn>
2021-12-14 17:42:34 +0800

    add pic time

---
+++ ios/Classes/Tools/CFQTools.h
@@ -0,0 +1,17 @@
+//
+//  CFQTools.h
+//  fulive_plugin
+//
+//  Created by caofuqing on 2021/12/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CFQTools : NSObject
+//获取当前时间戳
++ (NSString *)currentTimeStr;
+@end
+
+NS_ASSUME_NONNULL_END
